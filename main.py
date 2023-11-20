0ea1f76d90d8146177f064bdcab4f75e0d16d829
travkinei <travkinei@yandexlyceum.ru>
2023-11-20 11:06:29 +0400

    no first commit

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 def main():
     print('My first git program')
     print('My first git program')
+    print('My first git program')
+    s = 'sdfgh'
+    print(s.capitalize())
 
 
 if __name__ == '__main__':
